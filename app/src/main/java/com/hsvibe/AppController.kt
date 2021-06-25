643eb222a85cb184c646d464d55c5b63c15fcdf6
Vincent Chang <brack.clare@gmail.com>
2021-06-25 17:43:44 +0800

    Init commit.

---
+++ app/src/main/java/com/hsvibe/AppController.kt
@@ -0,0 +1,37 @@
+package com.hsvibe
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+
+/**
+ * Created by Vincent on 2021/6/25.
+ */
+class AppController : MultiDexApplication() {
+
+    companion object {
+        @get:Synchronized
+        lateinit var instance: AppController
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.applicationWindowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+    }
+
+}
